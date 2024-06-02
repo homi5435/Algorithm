3e97ae92636486d7ef33e8a372dee451910e49ef
homi5435 <hhss5435@gmail.com>
2024-06-02 20:02:11 +0900

    코딩 테스트 준비 백준 시작

---
+++ src/main/java/IO/IO_1008.java
@@ -0,0 +1,13 @@
+package IO;
+
+import java.util.Scanner;
+
+public class IO_1008 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double A, B;
+        A = sc.nextDouble();
+        B = sc.nextDouble();
+        System.out.println(A / B);
+    }
+}
