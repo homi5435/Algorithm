160714834125a576c99bf93654c2fe6f7d3ab36a
homi5435 <hhss5435@gmail.com>
2024-06-21 13:02:38 +0900

    백준 단계별 약수,배수 문제 풀이 소수

---
+++ src/main/java/Dm/Dm_2581.java
@@ -0,0 +1,52 @@
+package Dm;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class Dm_2581 {
+
+    public static boolean[] prime;
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int m = Integer.parseInt(br.readLine());
+        int n = Integer.parseInt(br.readLine());
+
+        prime = new boolean[n + 1];
+        get_prime();
+
+        int sum = 0;
+        int min = Integer.MAX_VALUE;
+        for(int i = m; i <= n; i++) {
+            if(!prime[i]) {
+                sum += i;
+                if(min == Integer.MAX_VALUE) {
+                    min = i;
+                }
+            }
+        }
+
+        if(sum == 0) {
+            System.out.println(-1);
+        }
+        else {
+            System.out.println(sum);
+            System.out.println(min);
+        }
+
+    }
+    public static void get_prime() {
+        prime[0] = true;
+        prime[1] = true;
+
+        for(int i = 2; i <= Math.sqrt(prime.length); i++) {
+            if(prime[i]) continue;
+            for(int j = i * i; j < prime.length; j += i) {
+                prime[j] = true;
+            }
+        }
+
+    }
+}
