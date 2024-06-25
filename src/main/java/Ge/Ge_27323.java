02fc3200140296357c485cc5c5b5322198fb49a7
homi5435 <hhss5435@gmail.com>
2024-06-25 16:22:16 +0900

    백준 단계별 기하 직사각형의 넓이 문제

---
+++ src/main/java/Ge/Ge_27323.java
@@ -0,0 +1,16 @@
+package Ge;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Ge_27323 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int x = Integer.parseInt(br.readLine());
+        int y = Integer.parseInt(br.readLine());
+        int r = x * y;
+        System.out.println(r);
+    }
+}
