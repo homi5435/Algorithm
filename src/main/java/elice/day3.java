f1a181b07de3eae73e23f8cad6325ab0000fb029
유호성 <hhss5435@gmail.com>
2024-07-11 20:21:21 +0900

    엘리스 코드 챌린지 3일차 - 문자열 압축 해제 재도전

--- src/main/java/elice/day3.java
+++ src/main/java/elice/day3.java
@@ -3,45 +3,42 @@ package elice;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Stack;
 
 public class day3 {
     public static void main(String[] args)throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String st = br.readLine();
 
-        int len = plength(st, 0, st.length());
-        System.out.println(len);
-        }
-    private static int plength(String s, int start, int end) {
-        int pdlength = 0;
-        int i = start;
+        Stack<Character> stack = new Stack<>();
+        int[] depthResult = new int[50];
+        int depth = 0;
 
-        while (i < end) {
-            char c = s.charAt(i);
-            if (Character.isDigit(c)) { //char값의 숫자인지 문자인지 판독
-                int num = c - '0';
-                i++;
-                int subStart = i + 1;
-
-                int bracketCount = 1;
-                int subEnd = subStart;
-                while (bracketCount > 0) {
-                    subEnd++;
-                    if (s.charAt(subEnd) == '(') {
-                        bracketCount++;
-                    } else if (s.charAt(subEnd) == ')') {
-                        bracketCount--;
+        for (char ch : st.toCharArray()){
+            if (ch != ')'){
+                if (ch == '('){
+                    depth += 1;
+                    depthResult[depth] = 0;
+                }
+                stack.push(ch);
+            } else {
+                for (int i = stack.size() - 1; i >= 0; i--){
+                    if (stack.get(i) == '(') {
+                        int num = depthResult[depth];
+                        for (int j = i + 1; j < stack.size(); j++) {
+                            num += 1;
+                        }
+                        depth -= 1;
+                        depthResult[depth] += num * Character.getNumericValue(stack.get(i - 1));
+                        while (stack.size() > i - 1) {
+                            stack.pop();
+                        }
+                        break;
                     }
                 }
-                int subLength = plength(s, subStart, subEnd); //재귀적으로 계산
-                pdlength += num * subLength;
-                i = subEnd;
-            } else{
-                pdlength++; //숫자가 아닌 문자일 경우 길이를 1 증가
             }
-            i++;
         }
-        return pdlength;
+        System.out.println(depthResult[0] + stack.size());
     }
 }
 
