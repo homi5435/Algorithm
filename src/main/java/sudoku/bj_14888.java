037847195ee696e25b2602c41ad3e2cb12ed14c7
유호성 <hhss5435@gmail.com>
2024-07-24 18:18:54 +0900

    백준 14888 - 연산자 끼워넣기 재귀 호출

---
+++ src/main/java/sudoku/bj_14888.java
@@ -0,0 +1,69 @@
+package sudoku;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class bj_14888 {
+
+    public static int max = Integer.MIN_VALUE;	// 최댓값
+    public static int min = Integer.MAX_VALUE;	// 최솟값
+    public static int[] op = new int[4];	// 연산자 개수
+    public static int[] num;					// 숫자
+    public static int n;						// 숫자 개수
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        n = Integer.parseInt(br.readLine());
+        num = new int[n];
+
+        // 숫자 입력
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            num[i] = Integer.parseInt(st.nextToken());
+        }
+
+        // 연산자 입력
+        st = new StringTokenizer(br.readLine(), " ");
+        for (int i = 0; i < 4; i++) {
+            op[i] = Integer.parseInt(st.nextToken());
+        }
+
+        dfs(num[0], 1);
+
+        System.out.println(max);
+        System.out.println(min);
+
+    }
+
+    public static void dfs(int num, int idx) {
+        if (idx == n) {
+            max = Math.max(max, num);
+            min = Math.min(min, num);
+            return;
+        }
+
+        for (int i = 0; i < 4; i++) {
+            // 연산자 개수가 1개 이상인 경우
+            if (op[i] > 0) {
+
+                // 해당 연산자를 1 감소시킨다.
+                op[i]--;
+
+                switch (i) {
+
+                    case 0:	dfs(num + num[idx], idx + 1);	break;
+                    case 1:	dfs(num - num[idx], idx + 1);	break;
+                    case 2:	dfs(num * num[idx], idx + 1);	break;
+                    case 3:	dfs(num / num[idx], idx + 1);	break;
+
+                }
+                // 재귀호출이 종료되면 다시 해당 연산자 개수를 복구한다.
+                op[i]++;
+            }
+        }
+    }
+
+}
