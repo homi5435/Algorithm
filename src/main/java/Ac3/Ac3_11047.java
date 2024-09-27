00592ffd06893ebe1f804764ea1baa4804cba7b3
[CLOUD4] 유호성 <hhss5435@gmail.com>
2024-09-27 16:55:50 +0900

    solved.ac - 11047 동전 0 그리디 알고리즘 사용

---
+++ src/main/java/Ac3/Ac3_11047.java
@@ -0,0 +1,34 @@
+package Ac3;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Ac3_11047 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+
+        int[] coin = new int[n];
+
+        for(int i = 0; i < n; i++) {
+            coin[i] = Integer.parseInt(br.readLine());
+        }
+
+        int count = 0;
+
+        for(int i = n - 1; i >= 0; i--) {
+            if(coin[i] <= k) {
+                count += (k / coin[i]);
+                k = k % coin[i];
+            }
+        }
+
+        System.out.println(count);
+    }
+}
